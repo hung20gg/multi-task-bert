7655b4ff49581d535c5373d956263da85ddc2b04
hung20gg <quanghung20gg@gmail.com>
2024-02-04 15:32:46 +0700

    4labels

--- dataloader.py
+++ dataloader.py
@@ -31,7 +31,7 @@ class CreateDataset:
         encoded_dict = self.tokenizer.encode_plus(sent,
                                              add_special_tokens=True,
                                              max_length=self.max_length,
-                                             pad_to_max_length=True,
+                                             padding='max_length',
                                              truncation = True,
                                              return_attention_mask=True,
                                              return_tensors='pt')
@@ -57,53 +57,62 @@ class CreateDataset:
                                 )
     return self.data_loader
 
-  def __len__ (self):
-    plus1 = 0 if self.labels1.shape[0]%self.batch_size == 0 else 1
-    return self.labels1.shape[0]//self.batch_size +plus1
 
-class CreateMLMDataset:
-    def __init__ (self,sentences,model_name,batch_size=32,max_length=128):
-      self.tokenizer=AutoTokenizer.from_pretrained(model_name, use_fast=False)
-      self.batch_size=batch_size
-      self.model_name=model_name
-      self.sentences=np.array(sentences)
-      self.max_length=max_length
-      self.device=  torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class Create3HEADDataset:
+  def __init__ (self,sentences,labels1,labels2,labels3,model_name,batch_size=32,max_length=128):
+    self.tokenizer=AutoTokenizer.from_pretrained(model_name, use_fast=False)
+    self.batch_size=batch_size
+    self.model_name=model_name
+    self.sentences=np.array(sentences)
+    self.labels1 = labels1
+    self.labels2 = labels2
+    self.labels3 = labels3
+    self.max_length=max_length
+    self.device=  torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+  def encoder_generator(self):
+    # sentences= self.sentences
     
-    def encoder_generator(self):
-      # sentences= self.sentences
-      
-      input_ids = []
-      attention_masks =[]
-      max_pos = []
-      for sent in self.sentences:
-          encoded_dict = self.tokenizer.encode_plus(sent,
-                                              add_special_tokens=True,
-                                              max_length=self.max_length,
-                                              padding=True,
-                                              truncation = True,
-                                              return_attention_mask=True,
-                                              return_tensors='pt')
-          input_ids.append(encoded_dict['input_ids'])
-          attention_masks.append(encoded_dict['attention_mask'])
-          max_pos.append(torch.sum(encoded_dict['attention_mask']))
-          
-      # self.max_pos = torch.cat(max_pos,dim=0).type(torch.FloatTensor).to(DEVICE).reshape(-1,1)
-      self.input_ids = torch.cat(input_ids,dim=0).to(DEVICE)
-      self.attention_masks = torch.cat(attention_masks,dim=0).to(DEVICE)
-    def todataloader(self):
-      self.encoder_generator()
+    
+    sent_index = []
+    input_ids = []
+    attention_masks =[]
+    for index,sent in enumerate(self.sentences):
+        sent_index.append(index)
+        encoded_dict = self.tokenizer.encode_plus(sent,
+                                             add_special_tokens=True,
+                                             max_length=self.max_length,
+                                             padding='max_length',
+                                             truncation = True,
+                                             return_attention_mask=True,
+                                             return_tensors='pt')
+        input_ids.append(encoded_dict['input_ids'])
+        attention_masks.append(encoded_dict['attention_mask'])
+    self.input_ids = torch.cat(input_ids,dim=0).to(DEVICE)
+    self.attention_masks = torch.cat(attention_masks,dim=0).to(DEVICE)
+    self.labels1 = torch.tensor(self.labels1).type(torch.LongTensor).to(DEVICE)
+    self.labels2 = torch.tensor(self.labels2).type(torch.LongTensor).to(DEVICE)
+    self.labels3 = torch.tensor(self.labels3).type(torch.LongTensor).to(DEVICE)
+    self.sent_index = torch.tensor(sent_index).to(DEVICE)
+
+  def todataloader(self):
+    self.encoder_generator()
 
 
-      self.dataset = TensorDataset(self.input_ids, self.attention_masks)
-      # generator = torch.Generator(device=DEVICE)
-      self.data_loader = DataLoader(self.dataset,
-                                    # sampler=RandomSampler(self.dataset),
-                                    batch_size=self.batch_size,
+    self.dataset = TensorDataset(self.input_ids, self.attention_masks ,self.labels1,self.labels2,self.labels3)
+    # generator = torch.Generator(device=DEVICE)
+    self.data_loader = DataLoader(self.dataset,
+                                  # sampler=RandomSampler(self.dataset),
+                                  batch_size=self.batch_size,
+                                  shuffle=True
+                                  # generator = generator,
+                                )
+    return self.data_loader
 
-                                  )
-      return self.data_loader
   
+
 class DataCollatorHandMade:
     
     def __init__(self,model_name, mlm_prob = 0.3):
--- loss_function.py
+++ loss_function.py
@@ -168,42 +168,103 @@ class SMARTLoss1Label(nn.Module):
             # Reset noise gradients for next step
             noise = noise.detach().requires_grad_()
             
+class SMARTLoss3Label(nn.Module):
+    
+    def __init__(
+        self,
+        eval_fn: Callable,
+        loss_fn: Callable,
+        loss_last_fn: Callable = None, 
+        norm_fn: Callable = inf_norm, 
+        num_steps: int = 1,
+        step_size: float = 1e-3, 
+        epsilon: float = 1e-6,
+        noise_var: float = 1e-5,
+    ) -> None:
+        super().__init__()
+        self.eval_fn = eval_fn 
+        self.loss_fn = loss_fn
+        self.loss_last_fn = default(loss_last_fn, loss_fn)
+        self.norm_fn = norm_fn
+        self.num_steps = num_steps 
+        self.step_size = step_size
+        self.epsilon = epsilon 
+        self.noise_var = noise_var
+        
+    def forward(self, embed, state, input_mask,task='sent'):
+        noise = torch.randn_like(embed.float(), requires_grad=True) * self.noise_var
+        
+        if task == 'sent':
+          # Indefinite loop with counter 
+          for i in count():
+            # Compute perturbed embed and states 
+            embed_perturbed = embed + noise
+            state_perturbed, _, _ = self.eval_fn(embed_perturbed.long(),input_mask)
+            # Return final loss if last step (undetached state)
+            if i == self.num_steps:
+                return self.loss_last_fn(state_perturbed, state) 
+            # Compute perturbation loss (detached state)
+            loss = self.loss_fn(state_perturbed, state.detach())
+            # Compute noise gradient ∂loss/∂noise
+            noise_gradient, = torch.autograd.grad(loss, noise,allow_unused=True)
+            # Move noise towards gradient to change state as much as possible 
+            # Modify the computation of step to handle NoneType
+            if noise_gradient is not None:
+                step = noise + self.step_size * noise_gradient
+            else:
+                # Handle the case where noise_gradient is None, e.g., set step to noise
+                step = noise
+                
+            
+            # Normalize new noise step into norm induced ball 
+            step_norm = self.norm_fn(step)
+            noise = step / (step_norm + self.epsilon)
+            # Reset noise gradients for next step
+            noise = noise.detach().requires_grad_()
+            
             
           
-        
-        #     # Compute perturbed embed and states 
-        #     embed_perturbed = embed + noise*0.5 + noise2*0.5 
-        #     state_perturbed, state_perturbed2 = self.eval_fn(embed_perturbed.long(),input_mask)
-        #     # Return final loss if last step (undetached state)
-        #     if i == self.num_steps:
-        #         return self.loss_last_fn(state_perturbed, state) , self.loss_last_fn(state_perturbed2, state2)
-        #     # Compute perturbation loss (detached state)
-        #     loss = self.loss_fn(state_perturbed, state.detach())
-        #     loss2 = self.loss_fn(state_perturbed2, state2.detach())
-        #     # Compute noise gradient ∂loss/∂noise
-        #     noise_gradient, = torch.autograd.grad(loss, noise,allow_unused=True)
-        #     noise_gradient2, = torch.autograd.grad(loss2, noise2,allow_unused=True)
-        #     # Move noise towards gradient to change state as much as possible 
-        #     # Modify the computation of step to handle NoneType
-        #     if noise_gradient is not None:
-        #         step = noise + self.step_size * noise_gradient
-        #     else:
-        #         # Handle the case where noise_gradient is None, e.g., set step to noise
-        #         step = noise
-                
-        #     if noise_gradient2 is not None:
-        #         step2 = noise2 + self.step_size * noise_gradient2
-        #     else:
-        #         # Handle the case where noise_gradient is None, e.g., set step to noise
-        #         step2 = noise2 
-            
-        #     # Normalize new noise step into norm induced ball 
-        #     step_norm = self.norm_fn(step)
-        #     noise = step / (step_norm + self.epsilon)
-        #     # Reset noise gradients for next step
-        #     noise = noise.detach().requires_grad_()
-            
-        #     step_norm2 = self.norm_fn(step2)
-        #     noise2 = step2 / (step_norm2 + self.epsilon)
-        #     # Reset noise gradients for next step
-        #     noise2 = noise2.detach().requires_grad_()
+        elif task == 'clas':
+          for i in count():
+              # Compute perturbed embed and states 
+              embed_perturbed = embed + noise 
+              _,state_perturbed,_  = self.eval_fn(embed_perturbed.long(),input_mask)
+              # Return final loss if last step (undetached state)
+              if i == self.num_steps: 
+                  return self.loss_last_fn(state_perturbed, state) 
+              # Compute perturbation loss (detached state)
+              loss = self.loss_fn(state_perturbed, state.detach())
+              # Compute noise gradient ∂loss/∂noise
+              noise_gradient, = torch.autograd.grad(loss, noise,allow_unused=True)
+              if noise_gradient is not None:
+                  step = noise + self.step_size * noise_gradient
+              else:
+                  # Handle the case where noise_gradient is None, e.g., set step to noise
+                  step = noise  
+              # Normalize new noise step into norm induced ball 
+              step_norm = self.norm_fn(step)
+              noise = step / (step_norm + self.epsilon)
+              # Reset noise gradients for next step
+              noise = noise.detach().requires_grad_()
+        else:
+          for i in count():
+              # Compute perturbed embed and states 
+              embed_perturbed = embed + noise 
+              _,_,state_perturbed  = self.eval_fn(embed_perturbed.long(),input_mask)
+              # Return final loss if last step (undetached state)
+              if i == self.num_steps: 
+                  return self.loss_last_fn(state_perturbed, state) 
+              # Compute perturbation loss (detached state)
+              loss = self.loss_fn(state_perturbed, state.detach())
+              # Compute noise gradient ∂loss/∂noise
+              noise_gradient, = torch.autograd.grad(loss, noise,allow_unused=True)
+              if noise_gradient is not None:
+                  step = noise + self.step_size * noise_gradient
+              else:
+                  # Handle the case where noise_gradient is None, e.g., set step to noise
+                  step = noise  
+              # Normalize new noise step into norm induced ball 
+              step_norm = self.norm_fn(step)
+              noise = step / (step_norm + self.epsilon)
+              # Reset noise gradients for next step
+              noise = noise.detach().requires_grad_()
